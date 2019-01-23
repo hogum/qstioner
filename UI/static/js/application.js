e6c31d96ebff17482fceae7531a68eefff22df4c
root <mugoh.ks@gmail.com>
2019-01-23 08:10:49 +0300

    [Starts #163391872] Get user form inputs

--- UI/static/js/application.js
+++ UI/static/js/application.js
@@ -1,3 +1,6 @@
+const url = 'http://localhost:5000';
+// const url = 'https://qstionerv2-api-heroku.herokuapp.com/';
+
 function showNav() {
     let class_id = document.getElementById('navbar');
 
@@ -7,4 +10,17 @@ function showNav() {
     } else {
         class_id.className = "nav-nav";
     }
+}
+
+
+function registerUser() {
+    let regForm = document.getElementById("registration-form");
+
+    let firstname = regForm.elements['firstname'].value;
+    let lastname = regForm.elements['lastname'].value;
+    let username = regForm.elements['username'].value;
+    let othername = regForm.elements['othername'].value;
+    let email = regForm.elements['email'].value;
+    let password = regForm.elements['password'].value;
+    let phonenumber = regForm.elements['phonenumber'].value;
 }
