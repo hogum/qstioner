fa770f2774dfd0cfbba6a491aa9896a65c89e49b
root <mugoh.ks@gmail.com>
2019-01-23 08:19:18 +0300

    [Feature #163391872] Convert inputs to json strings

--- UI/static/js/application.js
+++ UI/static/js/application.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:5000';
+const url = 'http://localhost:5000/api/v1/';
 // const url = 'https://qstionerv2-api-heroku.herokuapp.com/';
 
 function showNav() {
@@ -23,4 +23,23 @@ function registerUser() {
     let email = regForm.elements['email'].value;
     let password = regForm.elements['password'].value;
     let phonenumber = regForm.elements['phonenumber'].value;
+
+    fetch(url + 'auth/register', {
+        method: 'post',
+        headers: {
+            "Content-type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify({
+            firstname: firstname,
+            lastname: "missing",
+            othername: "missing",
+            email: email,
+            username: username,
+            phonenumber: 123000,
+            isadmin: true,
+            password: password
+        })
+    }).then(function (response)) {
+        return response.json()
+    }
 }
